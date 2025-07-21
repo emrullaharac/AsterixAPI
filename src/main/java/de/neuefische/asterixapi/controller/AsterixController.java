018ff0e2a3fa64ec8957c7a8afc2741d8fb51761
Emrullah <arac.emrullah@gmail.com>
2025-07-21 10:33:18 +0200

    Add AsterixController and Character model with sample data

---
+++ src/main/java/de/neuefische/asterixapi/controller/AsterixController.java
@@ -0,0 +1,30 @@
+package de.neuefische.asterixapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.neuefische.asterixapi.model.Character;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/asterix")
+public class AsterixController {
+
+    @GetMapping("/characters")
+    public List<Character> getAllCharacters() {
+        return List.of(
+                new Character("1", "Asterix", 35, "Warrior"),
+                new Character("2", "Obelix", 35, "Supplier"),
+                new Character("3", "Miraculix", 60, "Druid"),
+                new Character("4", "Majestix", 60, "Chief"),
+                new Character("5", "Troubadix", 25, "Bard"),
+                new Character("6", "Gutemine", 35, "Chiefs Wife"),
+                new Character("7", "Idefix", 5, "Dog"),
+                new Character("8", "Geriatrix", 70, "Retiree"),
+                new Character("9", "Automatix", 35, "Smith"),
+                new Character("10", "Grockelix", 35, "Fisherman")
+        );
+    }
+}
---
+++ src/main/java/de/neuefische/asterixapi/model/Character.java
@@ -0,0 +1,4 @@
+package de.neuefische.asterixapi.model;
+
+public record Character(String id, String name, int age, String profession) {
+}
