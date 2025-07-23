b052e4b549fa2f76daa9cc00ca153cd5cf8ec544
Emrullah <arac.emrullah@gmail.com>
2025-07-23 11:00:03 +0200

    Add integration tests for character endpoints with MockMvc

--- src/test/java/de/neuefische/asterixapi/controller/AsterixControllerTest.java
+++ src/test/java/de/neuefische/asterixapi/controller/AsterixControllerTest.java
@@ -3,6 +3,7 @@ package de.neuefische.asterixapi.controller;
 import de.neuefische.asterixapi.model.Character;
 import de.neuefische.asterixapi.repository.CharacterRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -61,6 +62,65 @@ class AsterixControllerTest {
                                       "profession": "Warrior"
                                     }
                                   """
+                ));
+    }
+
+    @Test
+    void addCharacter_shouldReturnCharacter_whenCalledWithCharacterDtoObject() throws Exception {
+        //GIVEN
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/asterix/characters")
+                        .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                        .content("""
+                                  {
+                                    "name": "Obelix",
+                                    "age": 38,
+                                    "profession": "Supplier"
+                                  }
+                                """))
+                //THEN
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                  {
+                                    "name": "Obelix",
+                                    "age": 38,
+                                    "profession": "Supplier"
+                                  }
+                                """
+                ))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty());
+
+    }
+
+    @Test
+    void updateCharacter_shouldReturnUpdatedCharacter_whenCalledWithValidData() throws Exception {
+        //GIVEN
+        Character character = new Character("2", "Idefix", 4, "Dog");
+        repo.save(character);
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.put("/asterix/characters/2")
+                        .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                        .content("""
+                                {
+                                    "name": "Idefix",
+                                    "age": 5,
+                                    "profession": "Dog"
+                                }
+                                """
+                        ))
+                //THEN
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                {
+                                    "id": "2",
+                                    "name": "Idefix",
+                                    "age": 5,
+                                    "profession": "Dog"
+                                }
+                                """
                 ));
     }
 
